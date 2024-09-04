675dd186b43d0ac0c155b59ad714c9369b28f5f6
Ahmed Daas <ahmedhamadah973@gmail.com>
2024-09-04 11:20:38 +0400

    fix addons model

--- src/Addon.php
+++ src/Addon.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Addon extends Model
 {
     protected $fillable = ['addon_id', 'quantity', 'name'];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('chargebee.addons_table_name');
+
+        parent::__construct($attributes);
+    }
 }
